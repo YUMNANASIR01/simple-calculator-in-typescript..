cb9b9cc9e0f935b727e585da246f876030b2a0c1
YUMNANASIR01 <160057966+YUMNANASIR01@users.noreply.github.com>
2024-03-19 14:06:55 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([
+    { message: "Enter your first number", type: "number", name: "firstNumber" },
+    { message: "Enter your second number", type: "number", name: "secondNumber" },
+    {
+        message: "select one of operators to perform action",
+        type: "list",
+        name: "operator",
+        choices: ["ADDITION", "SUBTRACTION", "MULTIPLICATION", "DIVISION"],
+    },
+]);
+// CONDITIONAL STATEMENT
+if (answer.operator === "ADDITION") {
+    console.log(answer.firstNumber + answer.secondNumber);
+}
+else if (answer.operator === "SUBTRACTION") {
+    console.log(answer.firstNumber - answer.secondNumber);
+}
+else if (answer.operator === "MULTIPLICATION") {
+    console.log(answer.firstNumber * answer.secondNumber);
+}
+else if (answer.operator === "DIVISION") {
+    console.log(answer.firstNumber / answer.secondNumber);
+}
